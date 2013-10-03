64889aa003892984ff6df623835c0c1b9e4b3894
Sean Porter <portertech@gmail.com>
2013-10-03 10:57:44 -0700

    clean slate

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'getaddrinfo'
+maintainer       'Heavy Water Operations, LLC.'
+maintainer_email 'support@hw-ops.com'
+license          'Apache 2.0'
+description      'Installs/Configures getaddrinfo'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
